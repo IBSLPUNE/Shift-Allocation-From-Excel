035eaf25b9d1205628eefe2bc6be83e8a821ab6e
Lucky Tamrakar <luckytamrakar.01@gmail.com>
2025-08-11 12:06:19 +0530

    added functionality of button

--- shift_allocation_from_excel/public/js/download_button.js
+++ shift_allocation_from_excel/public/js/download_button.js
@@ -1,26 +1,24 @@
 frappe.ui.form.on('Shift Allocation Tool', {
-    refresh(frm) {
-        frm.add_custom_button("Download Template", () => {
-        
-            if (!frm.doc.from_date || !frm.doc.to_date) {
-                frappe.msgprint(__('Please set Start Date and End Date before downloading the template.'));
-                return;
-            }
-            if (frm.is_dirty()) {
-                frappe.msgprint(__('Please save the form first before downloading the template.'));
-                return;
-            }
+    download_template(frm) {
+        if (!frm.doc.from_date || !frm.doc.to_date) {
+            frappe.msgprint(__('Please set Start Date and End Date before downloading the template.'));
+            return;
+        }
+        if (frm.is_dirty()) {
+            frappe.msgprint(__('Please save the form first before downloading the template.'));
+            return;
+        }
 
-            const startDate = frm.doc.from_date;
-            const endDate = frm.doc.to_date;
-            const docname = frm.doc.name;
+        const startDate = frm.doc.from_date;
+        const endDate = frm.doc.to_date;
+        const docname = frm.doc.name;
 
-            const url = `/api/method/shift_allocation_from_excel.download.download_shift_template?start_date=${startDate}&end_date=${endDate}&docname=${docname}`;
+        const url = `/api/method/shift_allocation_from_excel.download.download_shift_template?start_date=${startDate}&end_date=${endDate}&docname=${docname}`;
 
-            window.open(url, "_blank");
-        });
+        window.open(url, "_blank");
     }
 });
+
 
 
 
